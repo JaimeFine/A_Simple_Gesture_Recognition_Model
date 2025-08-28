9671a1cb760530912b63e7d509020570e831f0a5
JaimeFine <jaimefine6@gmail.com>
2025-08-28 15:03:31 +0800

    Update training_program.py

--- Camera/SVM/training_program.py
+++ Camera/SVM/training_program.py
@@ -10,11 +10,10 @@ thifinger = np.load("/home/JFan/gesture_recognition_pack/3finger.npy")
 foufinger = np.load("/home/JFan/gesture_recognition_pack/4finger.npy")
 fiffinger = np.load("/home/JFan/gesture_recognition_pack/5finger.npy")
 
-x = np.vstack([hand,fist,thumb,secfinger,thifinger,foufinger,fiffinger]).reshap>
-y = ([0]*len(hand)+[1]*len(fist)+[2]*len(thumb)+[3]*len(secfinger)+[4]*len(thif>
+x = np.vstack([hand,fist,thumb,secfinger,thifinger,foufinger,fiffinger]).reshape(-1, 42)
+y = ([0]*len(hand)+[1]*len(fist)+[2]*len(thumb)+[3]*len(secfinger)+[4]*len(thifinger)+[5]*len(foufinger)+[6]*len(fiffinger))
 
 model = SVC(kernel='linear')
 model.fit(x, y)
 print("Model trained")
-pickle.dump(model, open("/home/JFan/gesture_recognition_pack/svm_model.pkl", "w>
-
+pickle.dump(model, open("/home/JFan/gesture_recognition_pack/svm_model.pkl", "wb"))
