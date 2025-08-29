6ee41c89e130a0b8fafbd92a27621a75845a8c31
JaimeFine <jaimefine6@gmail.com>
2025-08-29 19:29:33 +0800

    Update gesture_recognition.py

--- Camera/SVM/gesture_recognition.py
+++ Camera/SVM/gesture_recognition.py
@@ -4,21 +4,25 @@ import mediapipe as mp
 import numpy as np
 import pickle
 
-model = pickle.load(open("/home/JFan/gesture_recognition_pack/svm_model.pkl", "rb"))
+model = pickle.load(open("/home/JFan/gesture_recognition_pack/svm_model.pkl", "rb"))	# Loading the trained model
 
+# Initialize Mediapipe for the hand tracking
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.5)
 mp_draw = mp.solutions.drawing_utils
 
+# Initialize picamera
 picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480), "format": "RGB888"}))
 picam2.start()
+
 while True:
 	frame = picam2.capture_array()
 	frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGB)
 	results = hands.process(frame_rgb)
 	frame_bgr = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
 
+	# Matching and Recognizing hand gestures
 	if results.multi_hand_landmarks:
 		for hand_landmarks in results.multi_hand_landmarks:
 			mp_draw.draw_landmarks(frame_bgr, hand_landmarks, mp_hands.HAND_CONNECTIONS)
